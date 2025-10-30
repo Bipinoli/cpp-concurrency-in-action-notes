85491da41793476635c75628d1769a84c3e623c3
boli <bipinoli90@gmail.com>
2025-10-30 19:11:35 +0100

    WIP - custom thread pool to sort batch of vectors using quicksort

--- chapter4_synchronizing_concurrent_operations/quick_sort/concurrent.hpp
+++ chapter4_synchronizing_concurrent_operations/quick_sort/concurrent.hpp
@@ -3,11 +3,20 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <thread>
 #include <mutex>
 #include <conditon_variable>
 #include <memory>
+#include <cassert>
 
 namespace concurrent {
+
+
+struct TaskStatus {
+  int id;
+  int in_progress;
+  unique_ptr<TaskStatus> parent;
+};
 
 struct Task {
   int start_index;
@@ -16,16 +25,86 @@ struct Task {
   bool die;
 };
 
-struct TaskStatus {
-  int in_progress;
-  unique_ptr<TaskStatus> parent;
-};
-
 struct PivotResult {
   bool pivoted;
   int pivot_boundry_left;
   int pivot_boundry_right;
 };
+
+class QuicksortWorkers {
+public:
+  //The workers must be joined in the main thread
+  vector<thread> workers;
+
+  for (int i=0; i < thread::hardware_concurrency() - 1; i++) {
+  QuicksortWorkers() {
+      this.workers.push_back(thread([&this](){
+        this.worker();
+      }));
+    }
+  }
+
+  bool submit_batch(vector<vector<int>>& nums_batch) {
+    //TODO: only allow submission if the whole batch is finished
+    //make sure that the status mutexes are note in locked state
+    this.nums_batch = nums_batch;
+    this.task_status = {};
+    // creating mutexes in-place with default constructor
+    this.task_status_mtx = vector<mutex>(nums_batch.size());
+    for (int i=0; i<nums_batch.size(); i++) {
+      this.task_status.push_back({
+        .id = i,
+        .in_progress = 1,
+        .parent = nullptr
+      });
+    }
+  }
+  void run_batch() {
+    
+  }
+private:
+  vector<vector<int>>& nums_batch;
+  queue<Task> task_q;
+  mutex task_q_mtx;
+  vector<TaskStatus> task_status;
+  vector<mutex> task_status_mtx;
+
+  void worker() {
+    while (true) {
+      Task task; 
+      {
+        // perhaps it is better to have a condition variable based mechanism here
+        // to avoid busy wait
+        lock_guard lk(this.task_q_mtx);
+        task = this.task_q.front(); 
+        this.task_q.pop();
+      }
+      if (task.die) {
+        return;
+      }
+
+      auto result = arrange_around_pivot(task_q.start_index, task_q.end_index, task_q.nums);
+      if (result.pivoted) {
+        scoped_lock lk(task_q_mtx, status_mtx);
+        assert(status.size() > task_q.id && status[task_q.id].empty());
+        task_q.push({
+          .start_index = task_q.start_index,
+          .end_index = result.pivot_boundry_left,
+          .nums = task_q.nums,
+          .die = false
+        });
+        task_q.push({
+          .start_index = result.pivot_boundry_right,
+          .end_index = task_q.end_index
+          .nums = task_q.nums,
+          .die = false
+        });
+
+      }
+    }
+  }
+
+}
 
 
 PivotResult arrange_around_pivot(const int start, const int end, vector<int>& nums) {
@@ -75,28 +154,5 @@ PivotResult arrange_around_pivot(const int start, const int end, vector<int>& nu
   };
 }
 
-void notify_task_finish() {
-
-}
-
-void quicksort(queue<Task>& task_q, mutex& task_q_mtx) {
-  while (true) {
-    Task task; 
-    {
-      lock_guard lk(task_q_mtx);
-      task = task_q.front(); 
-      task_q.pop();
-    }
-    if (task.die) {
-      return;
-    }
-
-    auto result = arrange_around_pivot(task_q.start_index, task_q.end_index, task_q.nums);
-    if (result.pivoted) {
-      
-    }
-
-  }
-}
 
 }
